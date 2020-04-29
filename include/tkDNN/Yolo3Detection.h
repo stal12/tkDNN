30f3f56f35e6deeaa685bab4261226139de4d542
Fabio Bagni <228594@studenti.unimore.it>
2020-04-29 20:30:13 +0200

    tkDNN with batch

--- include/tkDNN/Yolo3Detection.h
+++ include/tkDNN/Yolo3Detection.h
@@ -19,12 +19,15 @@ public:
     tk::dnn::Yolo* getYoloLayer(int n=0);
 
     cv::Mat bgr_h;
+
+    int batchID = 0;
     
 public:
     Yolo3Detection() {};
     ~Yolo3Detection() {}; 
 
     bool init(const std::string& tensor_path, const int n_classes=80);
+    void setBatchID(int id){batchID = id;}
     void preprocess(cv::Mat &frame);
     void postprocess();
 };
